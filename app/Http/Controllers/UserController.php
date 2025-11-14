cb19f95ce132e0611e6150fe1e639059187c4f4b
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2025-11-14 11:56:15 -0500

    Full creation user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();  //Is like findAll
+        return $users;
+    }
+
+    /**
+     * Not work ask AI
+     * */
+    public function searchUserNotUseOrm()
+    {
+        $users = DB::select(DB::raw("SELECT * FROM `users` where age = 30"));
+
+        return $users;
+
+    }
+
+    public function getUserWereAge()
+    {
+        // if (age == 30 && name like "D%") || (name Like "Jorge")
+        $user = User::where(column: "age", operator: "=", value: 30)
+            ->where("name", "LIKE", "D%")
+            ->orWhere("name", "Like", "Jorge")
+            ->orderBy("age", 'asc')
+            ->get();
+        return $user;
+    }
+
+
+    public function create()
+    {
+        //CREATE A USER VIA 1
+        $user = new User;
+        $user->name = 'Jorge';
+        $user->email = 'Jorge@gmail.com';
+        $user->password = Hash::make('123456');
+        $user->age = 18;
+        $user->address = "Reparto morales";
+        $user->zip_code = 290003;
+        $user->save();
+
+        //CREATE A USER V2
+        User::create([
+            'name' => 'Pepe',
+            'email' => 'Pepe@gmail.com',
+            'password' => Hash::make('123456'),
+            'age' => 15,
+            'address' => 'Reparto morales',
+            'zip_code' => 290423
+        ]);
+
+        //
+        User::create([
+            'name' => 'Cardivi',
+            'email' => 'Cardivi@gmail.com',
+            'password' => Hash::make('1234w56'),
+            'age' => 4,
+            'address' => 'Reparto camayo',
+            'zip_code' => 290423
+        ]);
+
+        return redirect()->route("user.index");
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route:<TYPE Petition(GET,POST,PUT,DELETE)> ("<Entrance Pointer>", [<ControllerCLass>,<Methode>] )
+Route::get("/", [UserController::class,"index"])->name("user.index");
+Route::get("/create", [UserController::class,"create"])->name("user.create");
+Route::get("/filter", [UserController::class,"getUserWereAge"])->name("user.filter");
+Route::get("/filter/manual",[UserController::class,"searchUserNotUseOrm"])->name("user.manual");
