261c0f4e28e69faa1b24fae8998a605925327399
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2025-11-16 15:10:10 -0500

    Season 7 [SEEDER, FACTORY, FAKER]: Create the FACTORY use Fake. Here you define a model for create a random entities for test your database

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Random\RandomException;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    /**
+     * @throws RandomException
+     */
+    public function definition(): array
+    {
+        //Used Faker
+        return [
+            'name' =>fake()->name(),//Generate a random of 25 character
+            'short_description' =>fake()->sentence(),
+            'description' => fake()->paragraph(3),
+            'price' => fake()->numberBetween(1, 150),
+        ];
+
+  //Used random values
+//        return [
+//            'name' => Str::random(25),//Generate a random of 25 character
+//            'short_description' =>Str::random(25),
+//            'description' =>Str::random(150),
+//            'price' => random_int(1,125),
+//        ];
+    }
+}
