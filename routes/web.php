ae126f012b2be5d5533828757280d52745d797c4
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2025-11-16 15:07:34 -0500

    Season 7 [SEEDER, FACTORY, FAKER]: Create the routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PeopleController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,12 @@ Route::get("/filter/manual",[UserController::class,"searchUserNotUseOrm"])->name
 /*
  * In this section I create a methods with dynamic params
  */
-Route::get("/product",[PeopleController::class,"index"])->name("product.index");
-Route::get("/product/show/{people}",[PeopleController::class,"findPeopleById"])->name("product.show");
+Route::get("/people",[PeopleController::class,"index"])->name("people.index");
+Route::get("/people/show/{people}",[PeopleController::class,"findPeopleById"])->name("people.show");
 //withoutMiddleware if I do not use that methode you have a 419 error
-Route::post('/product/insert', [PeopleController::class, 'insertPeople'])->withoutMiddleware(['web']);
-Route::delete('/product/delete/{id}', [PeopleController::class, 'deletePeopleById'])->withoutMiddleware(['web']);
-Route::put('/product/update/{id}', [PeopleController::class, 'updatePeopleById'])->withoutMiddleware(['web']);
+Route::post('/people/insert', [PeopleController::class, 'insertPeople'])->withoutMiddleware(['web']);
+Route::delete('/people/delete/{id}', [PeopleController::class, 'deletePeopleById'])->withoutMiddleware(['web']);
+Route::put('/people/update/{id}', [PeopleController::class, 'updatePeopleById'])->withoutMiddleware(['web']);
+
+//Product Route
+Route::get("/product",[ProductController::class,"index"])->name("product.index");
