d084ac1dec34ea24b6be9b392cf7e78dfd7fed77
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2025-11-16 18:02:52 -0500

    Season 8 [RELATION]: Create a model for Form with his Seeder for relation with the user 13

---
+++ database/seeders/PhoneSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Phone;
+use Illuminate\Database\Seeder;
+
+class PhoneSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Phone::create([
+            'prefix' => 34,
+            'phone_number' => 56155557,
+            'user_id' => 13
+        ]);
+    }
+}
