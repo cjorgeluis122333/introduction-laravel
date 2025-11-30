f81a935cf04fdda148bc3df3b93d8ebaffed256a
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2025-11-30 12:43:32 -0500

    Theme RELATION: Create the Request and Resource used in this sample

---
+++ app/Http/Resources/ServiceResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\relation\Service;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Service */
+class ServiceResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
